4e26989daa8811ccb4ee50cd45d8e5834ec4a3ca
ZoloMohan <mohanzolomon@gmail.com>
2019-07-28 01:03:34 +0530

    Updates

---
+++ precache-manifest.d10fbdf71202d51137ff2476e17d5f7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b0e6f0fb6a1f4d8e6d8a1b1e059956",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "6e61f19f90856f9c8545",
+    "url": "/todo-react/static/css/main.6c05fa10.chunk.css"
+  },
+  {
+    "revision": "aef52d10cccc15856332",
+    "url": "/todo-react/static/js/2.b8bdf457.chunk.js"
+  },
+  {
+    "revision": "6e61f19f90856f9c8545",
+    "url": "/todo-react/static/js/main.f0120ba3.chunk.js"
+  },
+  {
+    "revision": "cd203911fc85a7f73b39",
+    "url": "/todo-react/static/js/runtime~main.6292bb99.js"
+  }
+]);
